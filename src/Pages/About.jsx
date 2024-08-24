2a03722372c3236d331cce3c2f87b1e53ad74ac1
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-08-24 18:50:31 +0100

    added info into the about.jsx file

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,29 @@
 const About = () => {
-    
-    return 
-}
+  return (
+    <div>
+        <h1>About me</h1>
+      <p>
+        This is the page where you will get to know me. I am currently creating
+        a
+        <a href="https://kyle-lawrenson.github.io/blog/" target="_blank">
+          blog.
+        </a>
+      </p>
+      
+      <div class="currently">
+        <ul>
+          <li>
+            I am currently learning how to code with Codenation for the next 12
+            weeks, which I am excited about, from my home in England, Liverpool.{" "}
+          </li>
+          <li>
+            I will be experimenting with html for the time being in my free time
+          </li>
+          <li>I am interested in the coding behind video games.</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default About;
