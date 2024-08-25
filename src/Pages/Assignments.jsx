b24fe5671c8ee7a86f75a91b4877668663459c10
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-08-25 17:54:57 +0100

    added assignment links into assignments.jsx

--- src/Pages/Assignments.jsx
+++ src/Pages/Assignments.jsx
@@ -1,6 +1,80 @@
 const Assignments = () => {
     
-    return 
+    return (
+        <div>
+            <h1>CodeNation Assignments</h1>
+            <p>Here is all of my assignments I did whilst on the course, most recent at the top; </p>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/server-assignment" target="_blank">
+                Server Assignment Repository.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/markh-codenation/Cats4Lyfe" target="_blank">
+                Cats4Lyfe Group Assignment Repository.
+                </a>
+                <a href="https://cats4lyfe.netlify.app/" target="_blank">
+                Cats4Lyfe Group Assignment Live Link.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/calculator" target="_blank">
+                Calculator Assignment Repository.
+                </a>
+                <a href="https://kyle-lawrenson.github.io/calculator/" target="_blank">
+                Calculator Assignment Live Link.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/cyberpet" target="_blank">
+                Cyberpet Group Assignment Repository.
+                </a>
+                <a href="https://kyle-lawrenson.github.io/cyberpet/" target="_blank">
+                Cyberpet Group Assignment Live Link.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/DrumkitChallenge" target="_blank">
+                Drumkit Assignment Repository.
+                </a>
+                <a href="https://kyle-lawrenson.github.io/DrumkitChallenge/" target="_blank">
+                Drumkit Assignment Live Link.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/keyCodeChallenge" target="_blank">
+                Keycode Assignment Repository.
+                </a>
+                <a href="https://kyle-lawrenson.github.io/keyCodeChallenge/" target="_blank">
+                Keycode Assignment Live Link.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/website_clone_assignment" target="_blank">
+                Website Clone Assignment Repository.
+                </a>
+                <a href="https://kyle-lawrenson.github.io/website_clone_assignment/" target="_blank">
+                Website Clone Assignment Live Link.
+                </a>
+            </div>
+
+            <div>
+                <a href="https://github.com/Kyle-Lawrenson/flexboxNavBarChallenge" target="_blank">
+                Nav Bar Assignment Repository.
+                </a>
+                <a href="https://kyle-lawrenson.github.io/flexboxNavBarChallenge/" target="_blank">
+                Nav Bar Assignment Live Link.
+                </a>
+            </div>
+        </div>
+    )
 }
 
 export default Assignments;
--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -4,9 +4,9 @@ const Experience = () => {
     
     return (
         <div>
-            <h2>My experience</h2>
+            <h1>My experience</h1>
 
-<p>In college i learnt how to use; </p>
+<p>In college I learnt how to use the following software; </p>
 
 <div class="knowledge">
 
@@ -14,8 +14,7 @@ const Experience = () => {
 
         <h3>MAYA</h3> <a
             href="https://www.autodesk.co.uk/products/maya/overview?us_oa=dotcom-us&us_si=6d256227-35ee-40f8-91ef-1cf9b86f3370&us_pt=MAYAUL&us_at=Maya&term=1-YEAR&tab=subscription&plc=MAYA"
-            target="_blank"> <img className="logo" src={IMAGES.image1} alt="MAYA Logo"/>  </a>
-{/* width=70 length="70" */}
+            target="_blank"> <img className="logo" src={IMAGES.image1} alt="MAYA Logo"/></a>
     </div>
 
     <div class="photoshop">
@@ -23,14 +22,12 @@ const Experience = () => {
         <h3>Photoshop</h3> <a
             href="https://www.adobe.com/uk/products/photoshop/landpa.html?gclid=CjwKCAjw1emzBhB8EiwAHwZZxQAHq7I5GSYK8djZYX8Who6GUj8fqaim_kwKitFyNUIokcDrjFctTBoCQk8QAvD_BwE&mv=search&mv=search&mv2=paidsearch&sdid=2XBSBWBF&ef_id=CjwKCAjw1emzBhB8EiwAHwZZxQAHq7I5GSYK8djZYX8Who6GUj8fqaim_kwKitFyNUIokcDrjFctTBoCQk8QAvD_BwE:G:s&s_kwcid=AL!3085!3!441664377297!e!!g!!photoshop!1422700211!58647953511&gad_source=1"
             target="_blank"><img className="logo" src={IMAGES.image2} alt="Photoshop Logo" /></a>
-            {/* width=70 length="70" */}
 
     </div>
 
     <div class="ue4">
 
-        <h3>Unreal Engine 4</h3> <a href="https://www.unrealengine.com/en-US" target="_blank"><img className="logo02" src={IMAGES.image3} alt="Unreal Engine 4 logo" /> </a>
-{/* width=90 length="90" */}
+        <h3>Unreal Engine 4</h3> <a href="https://www.unrealengine.com/en-US" target="_blank"><img className="logo02" src={IMAGES.image3} alt="Unreal Engine 4 logo" /></a>
     </div>
 
 </div>
@@ -43,9 +40,6 @@ const Experience = () => {
         Photoshop. I had to pick a theme and I chose horror.</p>
 
         <img className="sdat-img" src={IMAGES.image4} /> 
-        
-
-
 </div>
         </div>
     )
